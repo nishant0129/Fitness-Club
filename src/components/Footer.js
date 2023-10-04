00718bc36b8c4b7bca9ad05c1a566b17333f9bca
Nishant Kumar <nishantkumar222555@gmail.com>
2023-10-05 00:50:03 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt="logo" width='200px' height='100px'/>
         <Typography variant='h5' pb='40px' mt='20px'>
-        Copyright © 2023 Anubhav Singh | All Rights Reserved
+        Copyright © 2023 Nishant Kumar | All Rights Reserved
         </Typography>
       </Stack>
     </Box>
